a58af306874bdb048e20644a2b0dde435b8d30ee
William Boman <william@redwill.se>
2022-12-21 19:03:46 +0100

    fix(validate-schema): update schema path

--- validate-schema/validate.sh
+++ validate-schema/validate.sh
@@ -8,4 +8,4 @@ set -euo pipefail
 
 npm install -g ajv ajv-cli ajv-formats
 
-xargs -n1 -I{} ajv validate -d {} --spec=draft2020 -c ajv-formats -s schemas/package-def.json -r 'schemas/{components,enums}/**/*.json' <<< "$PACKAGES"
+xargs -n1 -I{} ajv validate -d {} --spec=draft2020 -c ajv-formats -s schemas/package.schema.json -r 'schemas/{components,enums}/**/*.json' <<< "$PACKAGES"
