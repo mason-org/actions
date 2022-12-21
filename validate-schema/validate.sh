a9a2a8767c104d041545a660dfb641cd0fe137ae
William Boman <william@redwill.se>
2022-12-21 12:54:35 +0100

    feat: add validate-schema action

---
+++ validate-schema/validate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ -n $RUNNER_DEBUG ]]; then
+    set -x
+fi
+
+set -euo pipefail
+
+npm install -g ajv ajv-cli ajv-formats
+
+xargs -n1 -I{} ajv validate -d {} --spec=draft2020 -c ajv-formats -s schemas/package-def.json -r 'schemas/{components,enums}/**/*.json' <<< "$PACKAGES"
