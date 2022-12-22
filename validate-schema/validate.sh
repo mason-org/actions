72844db6fcec140eb70340479f3f2a130342da8e
William Boman <william@redwill.se>
2022-12-22 17:15:00 +0100

    fix(validate-schema): split xargs stdin on new lines

--- validate-schema/validate.sh
+++ validate-schema/validate.sh
@@ -8,4 +8,4 @@ set -euo pipefail
 
 npm install -g ajv ajv-cli ajv-formats
 
-xargs -n1 -I{} ajv validate -d {} --spec=draft2020 -c ajv-formats -s schemas/package.schema.json -r 'schemas/{components,enums}/**/*.json' <<< "$PACKAGES"
+<<< "$PACKAGES" tr ' ' '\n' | xargs -I{} ajv validate -d {} --spec=draft2020 -c ajv-formats -s schemas/package.schema.json -r 'schemas/{components,enums}/**/*.json'
