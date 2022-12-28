d7c45252f171d274c9cff4e135ba58b5155073f8
William Boman <william@redwill.se>
2022-12-28 18:53:52 +0100

    feat(validate-schema): ensure correct package name

--- validate-schema/validate.sh
+++ validate-schema/validate.sh
@@ -8,4 +8,14 @@ set -euo pipefail
 
 npm install -g ajv ajv-cli ajv-formats
 
-<<< "$PACKAGES" tr ' ' '\n' | xargs -I{} ajv validate -d {} --spec=draft2020 -c ajv-formats -s schemas/package.schema.json -r 'schemas/{components,enums}/**/*.json'
+<<< "$PACKAGES" tr ' ' '\n' | xargs -P10 -I{} ajv validate -d {} --spec=draft2020 -c ajv-formats -s schemas/package.schema.json -r 'schemas/{components,enums}/**/*.json'
+
+for pkg in $PACKAGES; do
+    pkg_name=$(sed -nE 's/name: (.+)$/\1/p' "$pkg")
+    if [[ "$pkg" != "packages/${pkg_name}/package.yaml" ]]; then
+        >&2 echo "$pkg_name name doesn't match directory name ($pkg)."
+        exit 1
+    else
+        echo "$pkg has valid package name $pkg_name"
+    fi
+done
