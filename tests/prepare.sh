57f7761119b4baebf4649dd6688613fb9e4fae39
William Boman <william@redwill.se>
2025-01-27 06:13:32 +0100

    fix(tests): set up php manually because of old versions on base images

--- tests/prepare.sh
+++ tests/prepare.sh
@@ -160,12 +160,8 @@ function install-ghc {
     echo "setup_haskell=true" >> "$GITHUB_OUTPUT"
 }
 
-function install-composer {
-    if [[ $RUNNER_OS == macOS ]]; then
-        brew install composer
-        return 0
-    fi
-    return 0
+function install-php {
+    echo "setup_php=true" >> "$GITHUB_OUTPUT"
 }
 
 function install-python {
@@ -187,7 +183,7 @@ match install-opam "pkg:opam"
 match install-r "packages/r-languageserver/package.yaml"
 match install-ruby "pkg:gem"
 match install-zstd "packages/zls/package.yaml"
-match install-composer "pkg:composer"
+match install-php "pkg:composer"
 match install-python "pkg:pypi"
 
 echo "SKIPPED_PACKAGES=${SKIPPED_PACKAGES[@]+"${SKIPPED_PACKAGES[@]}"}" >> "$GITHUB_ENV"
